73c0fa90fb51b9604638da4b56f39d5b18c5de76
nguyennamcn <namnqse171784@fpt.edu.vn>
2025-02-21 15:40:55 +0700

    fix

--- src/routes/navigate/stack.navigation.tsx
+++ src/routes/navigate/stack.navigation.tsx
@@ -1,22 +1,26 @@
+import { StyleSheet } from 'react-native'
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import ServiceProfileScreen from '../../screens/ServiceProfile';
 import BottomTabNavigator from './bottomTab.navigation';
-import DetailsScreen from '../../screens/DetailsScreen.js'; // Một màn hình chi tiết ví dụ
 
 const Stack = createStackNavigator();
 
-export default function StackNavigation() {
-  return (
-    <Stack.Navigator>
+const StackNavigation = () => {
+    return (
+      <Stack.Navigator>
       {/* Màn hình chính sẽ là BottomTabNavigator */}
       <Stack.Screen 
         name="Main" 
         component={BottomTabNavigator} 
         options={{ headerShown: false }} 
       />
-      
+
       {/* Màn hình khác ví dụ */}
-      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="Details" component={ServiceProfileScreen} />
     </Stack.Navigator>
-  );
+    )
 }
+export default StackNavigation
+
+const styles = StyleSheet.create({})
